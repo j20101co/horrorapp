8e29b8293d53827f0f6a65b4c001adec89a906cb
j20101co <c94585004@gmail.com>
2022-11-26 16:50:01 +0900

    multi_login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('mail')->unique();
+            $table->string('name');
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-	    $table->string('profile');
+	        $table->string('profile')->nullable();
         });
     }
 
